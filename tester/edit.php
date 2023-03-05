62b9072d75560db2e49a3722eb375f3dc1f155c5
vijigithub12 <seethaviji636941@gmail.com>
2023-03-05 05:47:41 -0800

    second commit

--- tester/edit.php
+++ tester/edit.php
@@ -70,7 +70,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Tester</title>
+    <title>sample</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js
 "></script>
--- tester/homepage.php
+++ tester/homepage.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['email'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tester</title>
+  <title>Sample</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 
 
